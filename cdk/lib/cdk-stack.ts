94b26a64f7e059113efc0e918013cd20aade31cb
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-16 12:34:22 -0400

    cfinc existing cloudformation templates to cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cfninc from 'aws-cdk-lib/cloudformation-include';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const mainTemplate = new cfninc.CfnInclude(this, 'MainStack', {
+      templateFile: '../cfn-templates/main.json',
+      loadNestedStacks: {
+        'Certificate': {
+          templateFile: '../cfn-templates/acmcert.json',
+        },
+        'Cloudfront': {
+          templateFile: '../cfn-templates/cloudfront.json',
+        },
+        'CustomResources': {
+          templateFile: '../cfn-templates/customresources.json',
+        },
+      },
+    });
   }
 }
