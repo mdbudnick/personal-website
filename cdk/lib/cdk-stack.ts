469e22d128612350dd3b1b801944b6a10fb71c1c
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-17 10:23:10 -0400

    Rebuild CDK code from scratch with DynamoDb table

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,39 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as cfninc from 'aws-cdk-lib/cloudformation-include';
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { CloudFrontToApiGatewayToLambda } from '@aws-solutions-constructs/aws-cloudfront-apigateway-lambda';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MyBlogAppStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const mainTemplate = new cfninc.CfnInclude(this, 'MainStack', {
-      templateFile: 'cfn-templates/main.yaml',
-      loadNestedStacks: {
-        'AcmCertificateStack': {
-          templateFile: 'cfn-templates/acmcert.yaml',
-        },
-        'CloudFrontStack': {
-          templateFile: 'cfn-templates/cloudfront.yaml',
-        },
-        'CustomResourceStack': {
-          templateFile: 'cfn-templates/customresources.yaml',
-        },
-      },
+    const blogTable = new dynamodb.Table(this, 'BlogPosts', {
+      partitionKey: { name: 'postId', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.RETAIN // Only for dev, use different policy for production.
     });
-
-    const acmCert = mainTemplate.getNestedStack('AcmCertificateStack').includedTemplate.getResource('Certificate');
-    
-
-    const cfApiAndLambda = new CloudFrontToApiGatewayToLambda(this, 'test-cloudfront-apigateway-lambda', {
-      lambdaFunctionProps: {
-        code: lambda.Code.fromAsset(`lambda`),
-        runtime: lambda.Runtime.NODEJS_16_X,
-        handler: 'index.handler'
-      }
-    });
-
   }
 }
+
