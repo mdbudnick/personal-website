19e46a4c6c7f23e317493ef6d5ce62fe371607f7
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-18 18:06:37 -0400

    Provide bucket and domain name to script

--- cdk/cdk-deploy-to.sh
+++ cdk/cdk-deploy-to.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ $# -ge 2 ]]; then
+if [[ $# -ge 4 ]]; then
     export CDK_DEPLOY_ACCOUNT=$1
     export CDK_DEPLOY_REGION=$2
+    export BUCKET_NAME=$3
+    export DOMAIN_NAME=$4
     shift; shift
     zip -r www.zip ../www
     npx cdk deploy "$@"
@@ -10,6 +12,7 @@ if [[ $# -ge 2 ]]; then
     exit $?
 else
     echo 1>&2 "Provide account and region as first two args."
+    echo 1>&2 "Provide bucket and domain name as third and fourth args, respectively."
     echo 1>&2 "Additional args are passed through to cdk deploy."
     exit 1
 fi
