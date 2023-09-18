0e365229d492702d5862f6d33f34e6057c6b2d4d
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-18 19:35:29 -0400

    Update readHandler and createHandler in lambda/index.ts and install deps

--- cdk/lambda/index.ts
+++ cdk/lambda/index.ts
@@ -8,14 +8,26 @@ import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Context
 type LambdaFunctionUrlEvent = APIGatewayProxyEventV2
 type LambdaFunctionUrlResult = APIGatewayProxyStructuredResultV2
 
-export async function handler(
-  event: LambdaFunctionUrlEvent,
-  context: Context
-): Promise<LambdaFunctionUrlResult> {
-  console.log(context.functionName)
-  console.log(`${event.requestContext.http.method} ${event.rawPath}`)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event, null, 2),
-  }
-}
+    export async function readHandler(
+    event: LambdaFunctionUrlEvent,
+    context: Context
+    ): Promise<LambdaFunctionUrlResult> {
+    console.log(context.functionName)
+    console.log(`${event.requestContext.http.method} ${event.rawPath}`)
+    return {
+        statusCode: 200,
+        body: 'Hello read!',
+    }
+    }
+
+export async function createHandler(
+    event: LambdaFunctionUrlEvent,
+    context: Context
+    ): Promise<LambdaFunctionUrlResult> {
+    console.log(context.functionName)
+    console.log(`${event.requestContext.http.method} ${event.rawPath}`)
+    return {
+        statusCode: 200,
+        body: 'Hello create!',
+    }
+    }
