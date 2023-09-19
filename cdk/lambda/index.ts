2ac65f51017796067122ef5dfd7b7d7732aaa647
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-18 21:32:22 -0400

    Add createHandler function

--- cdk/lambda/index.ts
+++ cdk/lambda/index.ts
@@ -13,6 +13,16 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
+interface BlogPost {
+    postId: string;       // Unique identifier for the post
+    tags: string[];       // List of tags associated with the post
+    title: string;        // Title of the blog post
+    html: string;         // HTML content of the blog post
+    created?: string;     // Timestamp when the post was created (optional for updates)
+    updated?: string;     // Timestamp when the post was last updated (optional for creates)
+  }
+  
+
 export const readHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     // Handle GET /posts request
@@ -87,15 +97,55 @@ export const readHandler = async (event: APIGatewayProxyEvent): Promise<APIGatew
   }
 };
 
-
-export async function createHandler(
-    event: LambdaFunctionUrlEvent,
-    context: Context
-    ): Promise<LambdaFunctionUrlResult> {
-    console.log(context.functionName)
-    console.log(`${event.requestContext.http.method} ${event.rawPath}`)
-    return {
-        statusCode: 200,
-        body: 'Hello create!',
+exports.createHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+      if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Blog post required' }),
+          };
+      }
+      const requestBody = JSON.parse(event.body);
+  
+      // Validate required fields
+      if (!requestBody.title || !requestBody.html) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ message: 'Title and HTML are required' }),
+        };
+      }
+  
+      // Determine if it's an update or create operation based on the presence of postId
+      const isUpdate = requestBody.postId !== undefined;
+  
+      const timestamp = new Date().toISOString();
+      const item: BlogPost = {
+        postId: isUpdate ? requestBody.postId : timestamp, // Use postId for update or timestamp for create
+        tags: requestBody.tags || [],
+        title: requestBody.title,
+        html: requestBody.html,
+        created: isUpdate ? undefined : timestamp,
+        updated: isUpdate ? timestamp : undefined,
+      };
+  
+      const params = {
+        TableName: 'BlogPosts',
+        Item: item,
+      };
+  
+      // Perform the put operation to insert or update the item in DynamoDB
+      await dynamoDb.put(params).promise();
+  
+      return {
+        statusCode: isUpdate ? 200 : 201,
+        body: JSON.stringify({ message: isUpdate ? 'Post updated' : 'Post created' }),
+      };
+    } catch (error) {
+      console.error('Error:', error);
+  
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' }),
+      };
     }
-    }
+  };
