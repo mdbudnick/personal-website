ce5ac229a6d7d09b43c2213c3284d8f5ae3eedf3
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-25 18:20:33 -0400

    Add tests for top-row

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,29 +1,90 @@
-describe('index.html', () => {
-  it('has an index.html page', () => {
-    cy.visit('http://localhost:3000/index.html')
-  })
-})
+const topRowHrefs = [
+  "index.html",
+  "certifications.html",
+  "blog.html",
+  "projects.html",
+];
 
-describe('404.html', () => {
-  it('has an 404.html page', () => {
-    cy.visit('http://localhost:3000/404.html')
-  })
-})
+const topRowContents = [
+  "about",
+  "certifications",
+  "blog",
+  "projects",
+];
 
-describe('blog.html', () => {
-  it('has a blog.html page', () => {
-    cy.visit('http://localhost:3000/blog.html')
-  })
-})
+describe("index.html", () => {
+  it("has an index.html page", () => {
+    cy.visit("http://localhost:3000/index.html");
+  });
 
-describe('projects.html', () => {
-  it('has an projects.html page', () => {
-    cy.visit('http://localhost:3000/projects.html')
-  })
-})
+  it("index.html title", () => {
+    cy.visit("http://localhost:3000/index.html");
+    cy.get("title").contains("Michael Budnick");
+  });
 
-describe('certifications.html', () => {
-  it('has an certifications.html page', () => {
-    cy.visit('http://localhost:3000/certifications.html')
-  })
-})
+  it("index.html title", () => {
+    cy.visit("http://localhost:3000/index.html");
+    cy.get("title").contains("Michael Budnick");
+  });
+
+  it("index.html top-row", () => {
+    cy.visit("http://localhost:3000/index.html");
+    cy.get(".top-row");
+    cy.get(".top-row .item").each(($el, i) => {
+      expect($el.attr("href")).equals(topRowHrefs[i]);
+      expect($el.text()).equals(topRowContents[i]);
+    });
+  });
+});
+
+describe("blog.html", () => {
+  it("has a blog.html page", () => {
+    cy.visit("http://localhost:3000/blog.html");
+  });
+
+  it("blog.html top-row", () => {
+    cy.visit("http://localhost:3000/blog.html");
+    cy.get(".top-row");
+    cy.get(".top-row .item").each(($el, i) => {
+      expect($el.attr("href")).equals(topRowHrefs[i]);
+      expect($el.text()).equals(topRowContents[i]);
+    });
+  });
+});
+
+describe("projects.html", () => {
+  it("has an projects.html page", () => {
+    cy.visit("http://localhost:3000/projects.html");
+  });
+
+  it("projects.html top-row", () => {
+    cy.visit("http://localhost:3000/projects.html");
+    cy.get(".top-row");
+    cy.get(".top-row .item").each(($el, i) => {
+      expect($el.attr("href")).equals(topRowHrefs[i]);
+      expect($el.text()).equals(topRowContents[i]);
+    });
+  });
+});
+
+describe("certifications.html", () => {
+  it("has an certifications.html page", () => {
+    cy.visit("http://localhost:3000/certifications.html");
+  });
+
+  it("certifications.html top-row", () => {
+    cy.visit("http://localhost:3000/certifications.html");
+    cy.get(".top-row");
+    cy.get(".top-row .item").each(($el, i) => {
+      expect($el.attr("href")).equals(topRowHrefs[i]);
+      expect($el.text()).equals(topRowContents[i]);
+    });
+  });
+});
+
+describe("404.html", () => {
+  it("has an 404.html page", () => {
+    cy.visit("http://localhost:3000/404.html");
+  });
+  
+});
