4d2abe17415b02fd4055f6cb45454a93d4067515
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-25 07:34:09 -0400

    Add environment namespacing to stack names

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -4,31 +4,33 @@ import * as cdk from "aws-cdk-lib";
 import { StaticWebsiteBucket } from "../lib/bucket-stack";
 import { MyWebsiteAppStack } from "../lib/website-stack";
 
-const environments = ["test", "development", "production"]
-
+const environments = ["test", "development", "production"];
 
 const app = new cdk.App();
-const environment = app.node.tryGetContext("environment")?.toString();
+const environment: string = app.node.tryGetContext("environment")?.toString();
 if (!environment) {
-  throw new Error("An environment must be passed on deploy")
+  throw new Error("An environment must be passed on deploy");
 } else if (!environments.includes(environment)) {
-  throw new Error("Valid environments are 'production' or 'development'")
+  throw new Error("Valid environments are 'production' or 'development'");
 }
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 require("dotenv").config({ path: `.env.${environment}` });
 
-const bucketStack = new StaticWebsiteBucket(app, "PersonalWebsiteBucket", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-  environment,
-  bucketName: process.env.BUCKET_NAME || "",
-});
+const bucketStack = new StaticWebsiteBucket(
+  app,
+  `${environment}-PersonalWebsiteBucket`,
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+    environment,
+    bucketName: process.env.BUCKET_NAME || "",
+  }
+);
 
-
-new MyWebsiteAppStack(app, "PersonalWebsite", {
+new MyWebsiteAppStack(app, `${environment}-PersonalWebsite`, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
