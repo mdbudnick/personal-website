2518b265fc8ce11b5dd02d699a783149508e217f
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-12-17 23:01:08 -0500

    Update cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -153,9 +153,10 @@ describe("blog.html", () => {
   });
 });
 */
-const projectHrefs = ["https://www.breathbox.net/", "#"];
 
-const projectNames = ["Breathbox", "This Website!"];
+const projectNames = ["High-Availability Home K8s Cluster", "Breathbox", "This Website!"];
+
+const projectHrefs = ["https://mdbudnick.github.io/home-k8s/", "https://github.com/mdbudnick/home-k8s/", "https://www.breathbox.net/", "https://github.com/mdbudnick/breathbox/", "https://github.com/mdbudnick/personal-website/", "https://github.com/mdbudnick/personal-website/blob/main/.github/workflows/prod_test_and_sync.yml"];
 
 describe("projects.html", () => {
   it("has an projects.html page", () => {
@@ -188,12 +189,20 @@ describe("projects.html", () => {
     cy.get(".top-row .avatar");
   });
 
-  it("projects.html projects", () => {
+  it("projects.html titles", () => {
     cy.visit("projects.html");
     cy.get(".projects .project .project-blurb .project-title").each(
       ($el, i) => {
+        expect($el.text()).equals(projectNames[i]);
+      }
+    );
+  });
+
+  it("projects.html links", () => {
+    cy.visit("projects.html");
+    cy.get(".projects .project .project-blurb a").each(
+      ($el, i) => {
         expect($el.attr("href")).equals(projectHrefs[i]);
-        expect($el.children("h2").text()).equals(projectNames[i]);
       }
     );
   });
