dadfb97f435dfe4e73e8cdb93c3e43803b35477c
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-11-27 10:07:05 -0500

    Change order

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -178,9 +178,8 @@ const projectHrefs = [
   "https://github.com/mdbudnick/personal-website/",
   // eslint-disable-next-line max-len
   "https://github.com/mdbudnick/personal-website/blob/main/.github/workflows/prod_test_and_sync.yml",
-  "https://mdbudnick.github.io/img-breathbox/",
-  "https://mdbudnick.github.io/img-breathbox/",
   "https://github.com/mdbudnick/img-breathbox/",
+  "https://mdbudnick.github.io/img-breathbox/",
 ];
 
 describe("projects.html", () => {
