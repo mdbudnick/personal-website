fc15eb9dbc4f89e7c90b6527819638688d28ac39
Michael Budnick <mdbudnick@users.noreply.github.com>
2025-06-22 11:44:18 -0400

    Update cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,5 @@
 /// <reference types="cypress" />
 
-const menuItemHrefs = ["index.html", "projects.html", "certifications.html"];
-
-const menuItemContents = ["about", "projects", "certifications"];
-
 const socialMediaHrefs = [
   "https://www.linkedin.com/in/michael-budnick-79a40b60",
   "https://github.com/mdbudnick",
@@ -14,37 +10,6 @@ const socialMediaImgs = [
   "img/linkedin.png",
   "img/github.png",
   "img/cv-download.png",
-  "img/light-dark.png",
-];
-
-const middlePointContents = [
-  "Full Stack Product Developer",
-  "Cloud-Native & DevOps Expert",
-  "UI/UX & API Integrator",
-  "End-to-End Solution Builder",
-];
-
-const mediaItems = ["Reading", "Listening", "Watching"];
-
-const mediaLinks = [
-  // eslint-disable-next-line max-len
-  "https://www.abebooks.com/9780593357774/Diet-Small-Planet-Revised-Updated-0593357779/plp",
-  // eslint-disable-next-line max-len
-  "https://open.spotify.com/show/5jojerqfKxYXc5POfxC1o9?si=a1059cba459f469d&nd=1&dlsi=a543505099704c19",
-  // eslint-disable-next-line max-len
-  "https://skillbuilder.aws/exam-prep/sysops-administrator-associate",
-];
-
-const mediaTitles = [
-  "Diet for a Small Planet",
-  "Battle Cry of Freedom: The Civil War Era",
-  "AWS Certified SysOps Administrator Course",
-];
-
-const mediaAuthors = [
-  "Frances Moore Lappé",
-  "James McPherson",
-  "AWS Training and Certification",
 ];
 
 function indexTests(path) {
@@ -53,58 +18,38 @@ function indexTests(path) {
     cy.get("title").should("contain", "Michael Budnick");
   });
 
-  it("menu", () => {
+  it("container structure", () => {
     cy.visit(path);
-    cy.get(".left-panel");
-    cy.get(".left-panel .item").each(($el, i) => {
-      cy.wrap($el).should("have.attr", "href", menuItemHrefs[i]);
-      cy.wrap($el).should("have.text", menuItemContents[i]);
-    });
+    cy.get(".container").should("exist");
+    cy.get(".container .left-panel").should("exist");
+    cy.get(".container .right-panel").should("exist");
+  });
 
-    cy.get(".left-panel .menu .third-party-sm a").each(($el, i) => {
+  it("social media links", () => {
+    cy.visit(path);
+    cy.get(".left-panel .third-party-sm a").each(($el, i) => {
       cy.wrap($el).should("have.attr", "href", socialMediaHrefs[i]);
       cy.wrap($el).should("have.attr", "target", "_blank");
       cy.wrap($el).find("img").should("have.attr", "src", socialMediaImgs[i]);
     });
   });
 
-  it("middle", () => {
-    cy.visit(path);
-    cy.get(".middle .about-me .name").should("contain", "Michael Budnick");
-
-    cy.get(".middle .points .point").each(($el, i) => {
-      cy.wrap($el).should("have.text", middlePointContents[i]);
-    });
-
-    cy.get(".middle .bio").each(($el) => {
-      cy.wrap($el).should(
-        "contain",
-        "Hello! My name is Michael Budnick. I am a Senior Software Developer " +
-          "specializing in full stack and product development"
-      );
-      cy.wrap($el).should(
-        "contain",
-        "Personally, I live in New York City and thrive on life's adventures!"
-      );
-    });
-  });
-
   it("avatar", () => {
     cy.visit(path);
-    cy.get(".left-panel .avatar");
+    cy.get(".left-panel .avatar").should("exist");
+    const avatarImg = cy.get(".left-panel .avatar");
+    avatarImg.should("have.attr", "src", "img/avatar-headshot.jpg");
   });
 
-  it("media-items", () => {
+  it("right panel content", () => {
     cy.visit(path);
-    cy.get(".current-media .media-item").each(($el, i) => {
-      cy.wrap($el)
-        .find("h2")
-        .should("have.text", "Currently " + mediaItems[i]);
-      const $mediaATag = $el.children("a");
-      cy.wrap($mediaATag).should("have.attr", "href", mediaLinks[i]);
-      cy.wrap($mediaATag).find("h3").should("contain", mediaTitles[i]);
-      cy.wrap($mediaATag).find("h3").should("contain", mediaAuthors[i]);
-    });
+    cy.get(".right-panel h1#name").should("contain", "Michael Budnick");
+    cy.get(".right-panel .middle").should("exist");
+  });
+
+  it("chat button container", () => {
+    cy.visit(path);
+    cy.get(".right-panel #chat-button-container").should("exist");
   });
 }
 
@@ -130,198 +75,122 @@ describe("blog.html", () => {
     cy.visit("blog.html");
   });
 
-  it("blog.html menu", () => {
+  it("blog.html container structure", () => {
     cy.visit("blog.html");
-    cy.get(".left-panel");
-    cy.get(".left-panel .item").each(($el, i) => {
-      expect($el.attr("href")).equals(menuItemHrefs[i]);
-      expect($el.text()).equals(menuItemContents[i]);
-    });
-
-    cy.get(".left-panel .menu .third-party-sm a").each(($el, i) => {
-      expect($el.attr("href")).equals(socialMediaHrefs[i]);
-      expect($el.attr("target")).equals("_blank");
-      expect($el.children("img").attr("src")).equals(socialMediaImgs[i]);
-    });
+    cy.get(".container").should("exist");
+    cy.get(".container .left-panel").should("exist");
+    cy.get(".container .right-panel").should("exist");
   });
 
-  it("blog.html middle", () => {
+  it("blog.html social media links", () => {
     cy.visit("blog.html");
-    cy.get(".middle h2").contains("Personal Blog");
-    cy.get(".middle .title").contains("Personal Blog");
+    cy.get(".left-panel .third-party-sm a").each(($el, i) => {
+      cy.wrap($el).should("have.attr", "href", socialMediaHrefs[i]);
+      cy.wrap($el).should("have.attr", "target", "_blank");
+      cy.wrap($el).find("img").should("have.attr", "src", socialMediaImgs[i]);
+    });
   });
 
   it("blog.html avatar", () => {
     cy.visit("blog.html");
-    cy.get(".left-panel .avatar");
+    cy.get(".left-panel .avatar").should("exist");
   });
 
-  it("blog.html blog", () => {
+  it("blog.html content", () => {
     cy.visit("blog.html");
-    cy.get(".blog");
-    cy.get(".blog .wordcloud");
-    cy.get(".blog .posts");
+    cy.get(".right-panel h1").should("exist");
+    cy.get(".right-panel .middle").should("exist");
+  });
+  
+  it("blog.html chat button", () => {
+    cy.visit("blog.html");
+    cy.get(".right-panel #chat-button-container").should("exist");
   });
 });
 */
 
-const projectNames = [
-  "Website Personal Chatbot",
-  "PiHole Kubernetes Ansible Configuration",
-  "High-Availability Home K8s Cluster",
-  "Breathbox App",
-  "Breathbox Website",
-  "This Website!",
-  "Kuychi Khipu Game",
-  "Breathbox CSS",
-];
-
-const projectHrefs = [
-  "https://github.com/mdbudnick/personal-website/blob/main/www/js/chat.js",
-  "https://github.com/mdbudnick/open-ai-lambda",
-  "https://github.com/mdbudnick/pihole-k8s-ansible",
-  "https://github.com/mdbudnick/pihole-k8s-ansible",
-  "https://mdbudnick.github.io/home-k8s/",
-  "https://mdbudnick.github.io/home-k8s/",
-  "https://github.com/mdbudnick/home-k8s/",
-  "https://www.breathbox.net/",
-  "https://play.google.com/store/apps/details?id=com.michaelbudnick.breathbox",
-  "https://github.com/mdbudnick/bb-react-native/",
-  "https://www.breathbox.net/",
-  "https://www.breathbox.net/",
-  "https://github.com/mdbudnick/breathbox/",
-  "#",
-  "https://github.com/mdbudnick/personal-website/",
-  // eslint-disable-next-line max-len
-  "https://github.com/mdbudnick/personal-website/blob/main/.github/workflows/prod_test_and_sync.yml",
-  "https://github.com/mdbudnick/khipugame",
-  "https://mdbudnick.github.io/khipugame/",
-  "https://github.com/mdbudnick/khipugame",
-  "https://mdbudnick.github.io/img-breathbox/",
-  "https://mdbudnick.github.io/img-breathbox/",
-  "https://github.com/mdbudnick/img-breathbox/",
-];
+// Project pages should be checked for general structure
+// but detailed content checks are removed as structure has changed
 
 describe("projects.html", () => {
   it("has an projects.html page", () => {
     cy.visit("projects.html");
   });
 
-  it("projects.html menu", () => {
+  it("projects.html container structure", () => {
     cy.visit("projects.html");
-    cy.get(".left-panel");
-    cy.get(".left-panel .item").each(($el, i) => {
-      cy.wrap($el).should("have.attr", "href", menuItemHrefs[i]);
-      cy.wrap($el).should("have.text", menuItemContents[i]);
-    });
+    cy.get(".container").should("exist");
+    cy.get(".container .left-panel").should("exist");
+    cy.get(".container .right-panel").should("exist");
+  });
 
-    cy.get(".left-panel .menu .third-party-sm a").each(($el, i) => {
+  it("projects.html social media links", () => {
+    cy.visit("projects.html");
+    cy.get(".left-panel .third-party-sm a").each(($el, i) => {
       cy.wrap($el).should("have.attr", "href", socialMediaHrefs[i]);
       cy.wrap($el).should("have.attr", "target", "_blank");
       cy.wrap($el).find("img").should("have.attr", "src", socialMediaImgs[i]);
     });
   });
 
-  it("projects.html middle", () => {
-    cy.visit("projects.html");
-    cy.get(".middle h2").contains("Projects");
-    cy.get(".middle .title").contains("Projects");
-  });
-
   it("projects.html avatar", () => {
     cy.visit("projects.html");
-    cy.get(".left-panel .avatar");
+    cy.get(".left-panel .avatar").should("exist");
   });
 
-  it("projects.html titles", () => {
+  it("projects.html content structure", () => {
     cy.visit("projects.html");
-    cy.get(".projects .project .project-blurb .project-title").each(
-      ($el, i) => {
-        cy.wrap($el).should("have.text", projectNames[i]);
-      }
-    );
+    cy.get(".right-panel").should("exist");
+    cy.get(".right-panel #chat-button-container").should("exist");
   });
 
-  it("projects.html links", () => {
+  it("projects.html project list", () => {
     cy.visit("projects.html");
-    cy.get(".projects .project .project-blurb a").each(($el, i) => {
-      cy.wrap($el).should("have.attr", "href", projectHrefs[i]);
-    });
+    // Check that project list exists with content
+    cy.get(".right-panel ul li a").should("have.length.at.least", 1);
   });
 });
 
-const certBadges = [
-  "img/aws-cloud-practitioner-badge.png",
-  "img/cka-badge.png",
-  "img/ckad-badge.png",
-  "img/cks-badge.png",
-  "img/tf-badge.png",
-  "img/aws-developer-badge.png",
-];
-
-const certTitles = [
-  "AWS Certified Cloud Practitioner",
-  "CKA: Certified Kubernetes Administrator",
-  "CKAD: Certified Kubernetes Application Developer",
-  "CKS: Certified Kubernetes Security Specialist",
-  "HashiCorp Certified: Terraform Associate (003)",
-  "AWS Certified Developer Associate",
-];
-
-const certCredlyLinks = [
-  "https://www.credly.com/badges/595b50eb-6538-4c75-bb56-b164b4d108a2",
-  "https://www.credly.com/badges/be49e46f-e2a4-48cd-aa0d-095076b29e0f",
-  "https://www.credly.com/badges/92e639c1-500e-400a-9a2e-cea433e3348d",
-  "https://www.credly.com/badges/9bd50578-bd01-4060-8895-1f3ef62cf62c",
-  "https://www.credly.com/badges/cef85466-bd22-4619-aa91-f3b9b74ac28c",
-  "https://www.credly.com/badges/ef695cda-e5dd-470f-b0d3-4010f41f21d3",
-];
+// Certification page now has a simpler structure
+// Detailed badge and link checks removed as the structure has changed
 
 describe("certifications.html", () => {
   it("has an certifications.html page", () => {
     cy.visit("certifications.html");
   });
 
-  it("certifications.html menu", () => {
+  it("certifications.html container structure", () => {
     cy.visit("certifications.html");
-    cy.get(".left-panel");
-    cy.get(".left-panel .menu .item").each(($el, i) => {
-      cy.wrap($el).should("have.attr", "href", menuItemHrefs[i]);
-      cy.wrap($el).should("have.text", menuItemContents[i]);
-    });
+    cy.get(".container").should("exist");
+    cy.get(".container .left-panel").should("exist");
+    cy.get(".container .right-panel").should("exist");
+  });
 
-    cy.get(".left-panel .menu .third-party-sm a").each(($el, i) => {
+  it("certifications.html social media links", () => {
+    cy.visit("certifications.html");
+    cy.get(".left-panel .third-party-sm a").each(($el, i) => {
       cy.wrap($el).should("have.attr", "href", socialMediaHrefs[i]);
       cy.wrap($el).should("have.attr", "target", "_blank");
       cy.wrap($el).find("img").should("have.attr", "src", socialMediaImgs[i]);
     });
   });
 
-  it("certifications.html middle", () => {
-    cy.visit("certifications.html");
-    cy.get(".middle h2").contains("Certifications");
-    cy.get(".middle .title").contains("Certifications");
-  });
-
   it("certifications.html avatar", () => {
     cy.visit("certifications.html");
-    cy.get(".left-panel .avatar");
+    cy.get(".left-panel .avatar").should("exist");
   });
 
-  it("certifications.html certs (.cert)", () => {
+  it("certifications.html content structure", () => {
     cy.visit("certifications.html");
-    cy.get(".cert .badge").each(($el, i) => {
-      cy.wrap($el).should("have.attr", "src", certBadges[i]);
-    });
+    cy.get(".right-panel").should("exist");
+    cy.get(".right-panel h1").should("exist");
+    cy.get(".right-panel #chat-button-container").should("exist");
+  });
 
-    cy.get(".cert .about").each(($el, i) => {
-      cy.wrap($el).children("h2").should("have.text", certTitles[i]);
-      cy.wrap($el)
-        .children("a")
-        .should("have.attr", "href", certCredlyLinks[i]);
-      cy.wrap($el).children("a").should("have.attr", "target", "_blank");
-      cy.wrap($el).children("a").should("have.text", "Credly");
-    });
+  it("certifications.html list", () => {
+    cy.visit("certifications.html");
+    // Check that certification list exists with content
+    cy.get(".right-panel ul li a").should("have.length.at.least", 1);
   });
 });
 
@@ -330,15 +199,15 @@ describe("404.html", () => {
     cy.visit("404.html");
   });
 
-  it("404.html menu", () => {
+  it("404.html container and structure", () => {
     cy.visit("404.html");
-    cy.get(".left-panel");
-    cy.get(".left-panel .menu .item").each(($el, i) => {
-      cy.wrap($el).should("have.attr", "href", menuItemHrefs[i]);
-      cy.wrap($el).should("have.text", menuItemContents[i]);
-    });
+    cy.get(".container").should("exist");
+    cy.get(".left-panel").should("exist");
+  });
 
-    cy.get(".left-panel .menu .third-party-sm a").each(($el, i) => {
+  it("404.html social media links", () => {
+    cy.visit("404.html");
+    cy.get(".left-panel .third-party-sm a").each(($el, i) => {
       cy.wrap($el).should("have.attr", "href", socialMediaHrefs[i]);
       cy.wrap($el).should("have.attr", "target", "_blank");
       cy.wrap($el).find("img").should("have.attr", "src", socialMediaImgs[i]);
@@ -356,9 +225,9 @@ describe("darkmode", () => {
   it("has darkmode button", () => {
     cy.visit("index.html");
     cy.get("body").should("have.class", "white-background");
-    cy.get(".light-dark").click();
+    cy.get(".third-party-sm .light-dark").click();
     cy.get("body").should("not.have.class", "white-background");
-    cy.get(".light-dark").click();
+    cy.get(".third-party-sm .light-dark").click();
     cy.get("body").should("have.class", "white-background");
   });
 });
